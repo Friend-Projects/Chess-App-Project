967f3a6dd5f57264f7cb04496cadb7886ba15780
Jason17401 <jason48.p@gmail.com>
2025-06-24 21:09:04 +1200

    Feat: Add Move model definition and edit Piece contents

--- src/main/java/com/friendprojects/chessapp/model/Move.java
+++ src/main/java/com/friendprojects/chessapp/model/Move.java
@@ -1,4 +1,46 @@
 package com.friendprojects.chessapp.model;
 
 public class Move {
+    private final Piece piece;
+    private final Position origin;
+    private final Position target;
+    private final Piece capturedPiece; // Optional as null
+    private final PieceType promotionType; // Optional as null
+
+    public Move(Piece piece, Position origin, Position target, Piece capturedPiece, PieceType promotionType) {
+        this.piece = piece;
+        this.origin = origin;
+        this.target = target;
+        this.capturedPiece = capturedPiece;
+        this.promotionType = promotionType;
+    }
+
+    public Piece getPiece() {
+        return this.piece;
+    }
+
+    public Position getOrigin() {
+        return this.origin;
+    }
+
+    public Position getTarget() {
+        return this.target;
+    }
+
+    public Piece getCapturedPiece() {
+        return this.capturedPiece;
+    }
+
+    public PieceType getPromotionType() {
+        return this.promotionType;
+    }
+
+    // TODO: Consider Captures, Disambiguating Moves, Promotion, Castling, Check
+    public String toAlgebraic() {
+        if (this.piece.getType() == PieceType.KNIGHT) {
+            return "N" + this.target.toAlgebraic();
+        } else {
+            return this.piece.getType().name().charAt(0) + this.target.toAlgebraic();
+        }
+    }
 }
--- src/main/java/com/friendprojects/chessapp/model/Piece.java
+++ src/main/java/com/friendprojects/chessapp/model/Piece.java
@@ -4,10 +4,22 @@ public class Piece {
 
     private final PieceType pieceType;
     private final Colour colour;
+    private Position position;
 
     public Piece(PieceType pieceType, Colour colour) {
         this.pieceType = pieceType;
         this.colour = colour;
+        this.position = null;
+    }
+
+    public Piece(PieceType pieceType, Colour colour, Position position) {
+        this.pieceType = pieceType;
+        this.colour = colour;
+        this.position = position;
+    }
+
+    public Position getPosition() {
+        return this.position;
     }
 
     public PieceType getType() {
@@ -20,5 +32,9 @@ public class Piece {
 
     public Colour getColour() {
         return this.colour;
+    }
+
+    public void setPosition() {
+        this.position = position;
     }
 }
