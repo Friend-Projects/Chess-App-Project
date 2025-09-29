54a57dfe53df59b8622abfbe78c1bc31b3475162
Jason17401 <jason48.p@gmail.com>
2025-09-29 23:09:16 +1300

    Feat: Add Expert AI logic

--- src/main/java/com/friendprojects/chessapp/ai/ExpertAI.java
+++ src/main/java/com/friendprojects/chessapp/ai/ExpertAI.java
@@ -1,12 +1,15 @@
 package com.friendprojects.chessapp.ai;
 
+import com.friendprojects.chessapp.ai.eval.Evaluator;
+import com.friendprojects.chessapp.ai.eval.Minimax;
 import com.friendprojects.chessapp.enums.Colour;
 import com.friendprojects.chessapp.model.Board;
 import com.friendprojects.chessapp.model.Move;
 
 public class ExpertAI implements AIStrategy {
+    private final Minimax minimax = new Minimax(8, Evaluator::combined, true);
     @Override
     public Move chooseMove(Colour colour, Board board) {
-        return null;
+        return minimax.getOptimalMove(colour, board);
     }
 }
--- src/main/java/com/friendprojects/chessapp/ai/eval/Minimax.java
+++ src/main/java/com/friendprojects/chessapp/ai/eval/Minimax.java
@@ -12,10 +12,18 @@ public class Minimax {
 
     private final int maxDepth;
     private final ToIntFunction<Board> evaluator;
+    private final boolean usePruning;
 
     public Minimax(int maxDepth, ToIntFunction<Board> evaluator) {
         this.maxDepth = maxDepth;
         this.evaluator = evaluator;
+        this.usePruning = false;
+    }
+
+    public Minimax(int maxDepth, ToIntFunction<Board> evaluator, boolean usePruning) {
+        this.maxDepth = maxDepth;
+        this.evaluator = evaluator;
+        this.usePruning = usePruning;
     }
 
     public Move getOptimalMove(Colour colour, Board board) {
@@ -28,7 +36,9 @@ public class Minimax {
             Board dummy = new Board(board);
             dummy.executeMove(legalMove);
             // n-ply = maxDepth - 1, as true 1-ply is the current legal moves iteration
-            int newValue = minimax(dummy, 0, this.maxDepth - 1, !isWhite);
+            int newValue = this.usePruning ?
+                    minimax(dummy, 0, this.maxDepth - 1, Integer.MIN_VALUE, Integer.MAX_VALUE, !isWhite)
+                    : minimax(dummy, 0, this.maxDepth - 1, !isWhite);
 
             if (isWhite && newValue > bestValue) {
                 bestValue = newValue;
@@ -64,6 +74,42 @@ public class Minimax {
                 dummy.executeMove(legalMove);
                 int value = minimax(board, currentDepth + 1, maxDepth, true);
                 minValue = Math.min(minValue, value);
+            }
+            return minValue;
+        }
+    }
+
+    private int minimax(Board board, int currentDepth, int maxDepth, int alpha, int beta, boolean isWhite) {
+        if (currentDepth == maxDepth) {
+            return evaluator.applyAsInt(board);
+        }
+        Colour colour = isWhite ? Colour.WHITE : Colour.BLACK;
+        List<Move> legalMoves = Rules.RULES_ENGINE.getLegalMoves(colour, board);
+
+        if (isWhite) {
+            int maxValue = Integer.MIN_VALUE;
+            for (Move legalMove : legalMoves) {
+                Board dummy = new Board(board);
+                dummy.executeMove(legalMove);
+                int value = minimax(board, currentDepth + 1, maxDepth, alpha, beta, false);
+                maxValue = Math.max(maxValue, value);
+                alpha = Math.max(alpha, maxValue);
+                if (beta <= alpha) {
+                    break;
+                }
+            }
+            return maxValue;
+        } else {
+            int minValue = Integer.MAX_VALUE;
+            for (Move legalMove : legalMoves) {
+                Board dummy = new Board(board);
+                dummy.executeMove(legalMove);
+                int value = minimax(board, currentDepth + 1, maxDepth, alpha, beta, true);
+                minValue = Math.min(minValue, value);
+                beta = Math.min(beta, minValue);
+                if (beta <= alpha) {
+                    break;
+                }
             }
             return minValue;
         }
